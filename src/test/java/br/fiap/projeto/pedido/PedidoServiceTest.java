46c895b49f32869f758a80e6e72f42d815e52e70
Richard Cardoso <richard.166@hotmail.com>
2024-01-25 21:47:34 -0300

    test: refactor 12

--- src/test/java/br/fiap/projeto/pedido/PedidoServiceTest.java
+++ src/test/java/br/fiap/projeto/pedido/PedidoServiceTest.java
@@ -170,10 +170,11 @@ public class PedidoServiceTest {
 
     @Test
     public void testComparePedidosComMesmaPrioridade() throws InvalidStatusException, NoItensException {
+        LocalDateTime time = LocalDateTime.now();
         Pedido pedido1 = new Pedido(UUID.randomUUID(), null, UUID.randomUUID(), StatusPedido.EM_PREPARACAO, 20d,
-                LocalDateTime.now());
+                time);
         Pedido pedido2 = new Pedido(UUID.randomUUID(), null, UUID.randomUUID(), StatusPedido.EM_PREPARACAO, 20d,
-                LocalDateTime.now());
+                time);
 
         int result = comparator.compare(pedido1, pedido2);
 
